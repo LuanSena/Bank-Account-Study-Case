77988e1da892d0bfb74a506852e41833d7925248
Luan Sena <luansena@live.com>
2022-01-05 15:55:32 -0300

    [Partial] Add account statement base structure

--- AccountOperator/AccountOperator/BusinessServices/Entities/Account.cs
+++ AccountOperator/AccountOperator/BusinessServices/Entities/Account.cs
@@ -6,30 +6,32 @@ public class Account
     
     public Guid Uid;
     public decimal Balance;
-    public string OwnerName;
     public List<StatementEntry> Statement;
 
     public Account(int balance, string ownerName)
     {
         Uid = new Guid();
         Balance = balance;
-        ownerName = ownerName;
     }
 
     public void Deposit(decimal amount)
     {
         var depositNetAmount = amount * (decimal) DepositFee;
         Balance += depositNetAmount;
+        Statement.Add(new StatementEntry(Uid, amount, "Deposit"));
     }
 
     public void Transfer(int amount, Account destination)
     {
         Balance -= amount;
         destination.Balance += amount;
+        Statement.Add(new StatementEntry(Uid, -amount, $"Transfer to {destination.Uid.ToString()}"));
+        //bug here, destionation accountin does not have statement entry
     }
     
     public void Withdrawn(int amount)
     {
         Balance -= amount;
+        Statement.Add(new StatementEntry(Uid, -amount, "Withdrawn"));
     }
 }
--- AccountOperator/AccountOperator/BusinessServices/Entities/StatementEntry.cs
+++ AccountOperator/AccountOperator/BusinessServices/Entities/StatementEntry.cs
@@ -2,5 +2,16 @@ namespace AccountOperator.BusinessServices.Entities;
 
 public class StatementEntry
 {
-    
+    public Guid AccountUid;
+    public decimal Amount;
+    public string OperationType;
+    public readonly DateTime OperationTime;
+
+    public StatementEntry(Guid accountUid, decimal amount, string operationType)
+    {
+        AccountUid = AccountUid;
+        amount = amount;
+        OperationType = operationType;
+        OperationTime = DateTime.Now;
+    }
 }
