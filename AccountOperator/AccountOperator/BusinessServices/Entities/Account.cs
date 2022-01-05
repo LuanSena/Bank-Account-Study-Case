8610d5c52ad83842ded2e9e5251184c0f352130a
Luan Sena <luansena@live.com>
2022-01-05 11:12:16 -0300

    Add account properties and deposit method

--- AccountOperator/AccountOperator/BusinessServices/Entities/Account.cs
+++ AccountOperator/AccountOperator/BusinessServices/Entities/Account.cs
@@ -2,5 +2,16 @@ namespace AccountOperator.BusinessServices.Entities;
 
 public class Account
 {
+    private const double DepositFee = 0.99;
     
+    public Guid Uid;
+    public decimal Balance;
+    public string OwnerName;
+
+    public decimal Deposit(decimal amount)
+    {
+        var depositNetAmount = amount * (decimal) DepositFee;
+        Balance += depositNetAmount;
+        return Balance;
+    }
 }
